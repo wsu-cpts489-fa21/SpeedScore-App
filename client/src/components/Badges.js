4ce4d42a8583d25995c631c04239ce3831258269
Adi Zangi <adi.zangi@wsu.edu>
2021-12-06 12:30:32 -0800

    Added finding the round time and strokes badges. Modified badge qualifications.

--- client/src/components/Badges.js
+++ client/src/components/Badges.js
@@ -13,36 +13,33 @@ const badges = {
                      description: "Played 30 rounds"}
    },
    roundTime: {
-      roundTimeLevel1: {qualification: 120,
-                        name: "Outlast a game",
-                        description: "Competed for 2 hours"},
-      roundTimeLevel2: {qualification: 240,
-                        name: "Outlast a long game",
-                        description: "Competed for 4 hours"},
-      roundTimeLevel3: {qualification: 360,
-                        name: "Never get tired",
-                        description: "Competed for 6 hours"}
+      roundTimeLevel1: {qualification: 180,
+                        name: "Speed gain",
+                        description: "Competed for less than 3 hours"},
+      roundTimeLevel2: {qualification: 60,
+                        name: "Speed gold",
+                        description: "Competed for less than 1 hour"}
    },
    roundStrokes: {
-      roundStrokesLevel1: {qualification: 10,
+      roundStrokesLevel1: {qualification: 90,
                            name: "Beginner strokes",
-                           description: "10 strokes in a round"},
-      roundStrokesLevel2: {qualification: 50,
+                           description: "90 or less strokes in a round"},
+      roundStrokesLevel2: {qualification: 80,
                            name: "Intermediate strokes",
-                           description: "50 strokes in a round"},
-      roundStrokesLevel3: {qualification: 100,
+                           description: "80 or less strokes in a round"},
+      roundStrokesLevel3: {qualification: 70,
                            name: "Master strokes",
-                           description: "100 strokes in a round"}
+                           description: "70 or less strokes in a round"}
    },
    roundsInMonth: {
       roundsInMonthLevel1: {qualification: 2,
                            name: "Occasional player",
                            description: "Played twice in a month"},
       roundsInMonthLevel2: {qualification: 4,
-                           name: "Intermediate frequency",
+                           name: "Medium perseverance",
                            description: "Played 4 times in a month"},
       roundsInMonthLevel3: {qualification: 8,
-                           name: "High frequency",
+                           name: "High perseverance",
                            description: "Played 8 times in a month"}
    }
 }
