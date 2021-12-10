fc6a4687fc5cdb10592f80d88a74d6f047bab9bd
jhorstma <jhorstma@eecs.wsu.edu>
2021-12-10 08:06:33 -0800

    Added functionality to add photos, videos, links to rounds. Also displays a main photo on the rounds table.

--- models/Round.js
+++ models/Round.js
@@ -1,15 +1,19 @@
 import mongoose from 'mongoose';
 
 const RoundSchema = new mongoose.Schema({
-    date: {type: Date, required: true},
-    course: {type: String, required: true},
-    type: {type: String, required: true, enum: ['practice','tournament']},
-    holes: {type: Number, required: true, min: 1, max: 18},
-    strokes: {type: Number, required: true, min: 1, max: 300},
-    minutes: {type: Number, required: true, min: 1, max: 240},
-    seconds: {type: Number, required: true, min: 0, max: 60},
-    notes: {type: String}
-  },
+  date: {type: Date, required: true},
+  course: {type: String, required: true},
+  type: {type: String, required: true, enum: ['practice','tournament']},
+  holes: {type: Number, required: true, min: 1, max: 18},
+  strokes: {type: Number, required: true, min: 1, max: 300},
+  minutes: {type: Number, required: true, min: 1, max: 240},
+  seconds: {type: Number, required: true, min: 0, max: 60},
+  pictures: [{type: String}],
+  mainPic: {type: String},
+  videos: [{type: String}],
+  videoLinks: [{type: String}],
+  notes: {type: String}
+},
   {
     toObject: {
     virtuals: true
